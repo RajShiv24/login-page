b1a940fbdb4453b1ec7b367a337749785fc08eb3
shivam <shivamsingh19164@gmail.com>
2025-11-14 15:55:21 +0530

    change style

---
+++ login-signup/src/components/loginsignup/LoginSignup.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './LoginSignup.css'
+import user_icon from '../assets/text.png'
+import email_icon from '../assets/email.png'
+import password_icon from '../assets/password.png'
+
+export const LoginSignup = () => {
+  return (
+    <div className='container'>
+        <div className="header">
+            <div className="text">Sign Up</div>
+            <div className="underline"></div>
+            </div>
+            <div className="input">
+            <div className="input">
+                <img src={user_icon} alt="" />
+                <input type="text"/>
+            </div>
+            <div className="input">
+                <img src={email_icon} alt="" />
+                <input type="email"/>
+            </div>
+            <div className="input">
+                <img src={password_icon} alt="" />
+                <input type="password"/>
+            </div>
+        </div>
+        <div className="forgot-password">Lost Password? <span>click here!</span></div>
+        <div className="submit-container">
+            <div className="submit">Sign Up</div>
+            <div className="submit">Login</div>
+        </div>
+    </div>
+  )
+}
